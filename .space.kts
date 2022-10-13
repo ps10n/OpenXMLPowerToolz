0c96a32f452c17e40b729c7aea795e73de7fa7e5
Ilya Delfi <xploit.d.ice@gmail.com>
2022-10-13 08:58:06 +0000

    New file .space.kts

---
+++ .space.kts
@@ -0,0 +1,26 @@
+// paths to tools
+val nuget = "C:\\TeamCity\\buildAgent\\tools\\NuGet.CommandLine.6.3.1"
+val msbuild = "C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\BuildTools\\MSBuild\\Current\\Bin\\MsBuild.exe"
+
+job("Build and test") {
+    host("Run nuget, msbuild, nunit") {
+        shellScript {
+            // 1. Restore dependencies
+            // 2. Build the solution (MySolution.sln)
+            // 3. Run tests in the MainTests.csproj project
+            // To escape spaces in command line, we use quotes e.g. "$msbuild"
+            content = """
+                "$nuget" restore
+                "$msbuild" netframework-sample.sln
+            """
+        }
+
+        // run this job only on
+        // a Windows worker
+        requirements {
+            os {
+                type = OSType.Windows
+            }
+        }
+    }
+}
